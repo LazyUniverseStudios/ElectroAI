b00b61f49f7dfe70c8908004446935959f0e88c5
ElektrifiedDev <160866233+ElektrifiedDev@users.noreply.github.com>
2026-08-16 13:00:51 +0100

    Added IDs to Config
    Adds centralized Discord IDs for the bot, server, channels, and roles, including the level role mapping.

--- src/config.py
+++ src/config.py
@@ -13,3 +13,52 @@ embedColor = {
     "MISC": 0x000001, # Miscellaneous embed color
     "BIRTHDAY": 0xFFD700 # Embed color for birthday messages
 }
+
+# Bot Identity IDs
+botIdentityIDs = {
+    "BOT": 1378449256049803384, # Bot ID for the Discord bot
+    "BOT_ADMIN": 757868967384711249  # Bot Admin ID for the Discord bot
+}
+
+# Server Identity IDs
+serverIdentityIDs = {
+    "GUILD": 1216146656878133429, # Guild ID for
+    "GUILD_OWNER": 757868967384711249  # Guild Owner ID for the Discord server
+}
+
+# Channel IDs
+channelIDs = {
+    "JOINS_CHANNEL": 1459155124089000072, # Joins Channel ID
+    "LEAVES_CHANNEL": 1459155124089000072, # Leaves Channel ID
+    "GENERAL_CHAT_CHANNEL": 1480599558642728990, # General Chat Channel ID
+    "INTRODUCTION_CHANNEL": 1537111461636218910, # Introduction Channel ID
+    "BIRTHDAY_CHANNEL": 1523831931710738544, # Birthday Channel ID
+    "QOTD_CHANNEL": 1523838557624471625, # Question of the Day Channel ID
+    "BOOSTS_CHANNEL": 1427728272560623626 # Boosts Channel ID
+}
+
+# Role IDs
+roleIDs = {
+    "BASE_MEMBER_ROLE": 1412101472438452306, # Base Member Role ID
+    "MEMBER_PING_SEPERATOR": 1412101886001020929, # Member Ping Seperator Role ID
+    "MEMBER_PROFILE_SEPERATOR": 1412101535227183176, # Member Profile Seperator Role ID
+    "BASE_BOT_ROLE": 1412101520786329711, # Base Bot Role ID
+    "LEVEL_ROLES": {
+        "LEVEL_5": 1538515520137986108, # Level 5
+        "LEVEL_10": 1412101177809829958, # Level 10
+        "LEVEL_20": 1412101132825657344, # Level 20
+        "LEVEL_30": 1412101119676518603, # Level 30
+        "LEVEL_40": 1412101106426970233, # Level 40
+        "LEVEL_50": 1412101089721057411, # Level 50
+        "LEVEL_60": 1412101074302537879, # Level 60
+        "LEVEL_70": 1412101053838786801, # Level 70
+        "LEVEL_80": 1412101030442958858, # Level 80
+        "LEVEL_90": 1412101011954208888, # Level 90
+        "LEVEL_100": 1412100991884460076, # Level 100
+        "LEVEL_110": 1412100964835528784, # Level 110
+        "LEVEL_120": 1412100943654158409, # Level 120
+        "LEVEL_130": 1412100917670449283, # Level 130
+        "LEVEL_140": 1412100893066788956, # Level 140
+        "LEVEL_150": 1412100878151979159, # Level 150
+    }
+}
